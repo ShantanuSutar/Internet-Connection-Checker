d549418597279e155c816b7b429b35b11e5e2325
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-18 23:20:46 +0530

    Running 10 second timer
    logic for making a request after every 10 second
    1. Setting a 10sec timer
    2.Decrementing it till 0sec
    3.At 0sec again checking connection
    4.Again starting the timer from 10sec

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 const popup = document.querySelector('.popup');
-let isOnline = true;
+let isOnline = true, timer = 11, intervalId;
 const checkConnection = async() => {
      try{
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -9,6 +9,8 @@ const checkConnection = async() => {
      }catch(error){
         isOnline = false;
      }
+     timer = 11;
+     clearInterval(intervalId)
      handlePopup(isOnline)
  }
 
@@ -17,6 +19,14 @@ const checkConnection = async() => {
       return popup.classList.remove('show');
    }
    popup.classList.add('show');
+   intervalId = setInterval(() => {
+      timer--;
+      if(timer === 0) {
+          checkConnection();
+      }
+      popup.querySelector(".about b").textContent = timer;
+       }, 1000);
+      
  }
 
-setInterval(checkConnection, 3000) 
+ setInterval(() => isOnline &&  checkConnection(), 3000)
